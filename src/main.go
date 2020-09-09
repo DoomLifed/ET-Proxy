57f9e15e47939de377c96d0f804f164ccaa62990
Morteza <DoomLifed@gmail.com>
2020-09-10 00:54:39 +0200

    Done

--- src/main.go
+++ src/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func copyResponseHeader(dst, src http.Header)  {
@@ -16,7 +20,7 @@ func copyResponseHeader(dst, src http.Header)  {
 
 func createHeader(header http.Header)  (http.Header, string){
 	var addr string
-	for key, _:= range header{
+	for key := range header{
 		if key == "Addr"{
 			addr = header.Get(key)
 			header.Del(key)
@@ -29,22 +33,64 @@ func createHeader(header http.Header)  (http.Header, string){
 	return header, addr
 }
 
+func doForwardRequestToAnotherProxy(request *http.Request, response http.ResponseWriter,proto , proxy string)  {
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+
+	}else {
+		proxyReq , err := http.NewRequest(request.Method, proto+"://"+proxy, bytes.NewReader(body))
+		proxyReq.Header = make(http.Header)
+		for h, val := range request.Header {
+			proxyReq.Header[h] = val
+		}
+		client := http.Client{}
+		res, err := client.Do(proxyReq)
+		if err != nil {
+			http.Error(response, err.Error(), http.StatusBadGateway)
+		}else {
+			defer res.Body.Close()
+			copyResponseHeader(response.Header(), res.Header)
+			response.WriteHeader(res.StatusCode)
+			_, _ = io.Copy(response, res.Body)
+		}
+	}
+
+}
 func inputRequest(response http.ResponseWriter, request *http.Request){
-	fmt.Println(request.RemoteAddr , request.RequestURI)
-	if request.URL.Scheme == "http" && request.URL.Scheme == "https"{
+	proto := strings.Split(request.Proto, "/")[0]
+	if proto != "HTTP" && proto != "HTTPS"{
 		http.Error(response, "Bad protocol " + request.URL.Scheme, http.StatusBadRequest)
 	}else {
 		if request.Header.Get("proxy") == "none"{
 			request.Header,  request.RemoteAddr= createHeader(request.Header)
 			client := http.Client{}
-			res, err := client.Do(request)
+			res , err := client.Do(request)
 			if err!=nil{
 				http.Error(response, err.Error(), res.StatusCode)
 			}else {
 				defer res.Body.Close()
 				copyResponseHeader(response.Header(), res.Header)
 				response.WriteHeader(res.StatusCode)
-				io.Copy(response, res.Body)
+				_, _ = io.Copy(response, res.Body)
+			}
+		}else {
+			proxy := strings.Split(request.Header.Get("proxy"), ",")
+			newValueOfProxy := ""
+			if len(proxy) > 1{
+				for i:=1 ; i<len(proxy) ; i++{
+					newValueOfProxy += proxy[i]
+					if i != len(proxy)-1{
+						newValueOfProxy += ","
+					}
+				}
+				request.Header.Set("proxy", newValueOfProxy)
+				doForwardRequestToAnotherProxy(request, response, proto, proxy[0])
+			}else {
+				fmt.Println(request.Header.Get("proxy"))
+				request.Header.Set("proxy", "none")
+				fmt.Println(request.Header.Get("proxy"))
+				doForwardRequestToAnotherProxy(request, response, proto, proxy[0])
 			}
 		}
 	}
@@ -52,5 +98,5 @@ func inputRequest(response http.ResponseWriter, request *http.Request){
 
 func main() {
 	http.HandleFunc("/", inputRequest)
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
